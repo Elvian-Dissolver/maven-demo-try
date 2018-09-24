22513fd30b8d9bc7d8161d689482dd308ad0ac18
DissoLver <indra.elvian@gmail.com>
2018-09-24 13:26:37 +0700

    Add constructor class ProductSeeder

--- src/main/java/models/ProductSeeder.java
+++ src/main/java/models/ProductSeeder.java
@@ -1,13 +1,20 @@
 package models;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
 public class ProductSeeder implements CommandLineRunner {
     private ProductRepository productRepository;
+
+    @Autowired
+    public ProductSeeder(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
     
     @Override
     public void run(String... args) throws Exception {
+
 
     }
 }
