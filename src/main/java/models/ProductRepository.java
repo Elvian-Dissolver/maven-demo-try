b35ea9a84a85291263e92982cc22460264da26c3
DissoLver <indra.elvian@gmail.com>
2018-09-24 13:21:54 +0700

    change ProductRepository class to extend

--- src/main/java/models/ProductRepository.java
+++ src/main/java/models/ProductRepository.java
@@ -1,4 +1,8 @@
 package models;
 
-public interface ProductRepository {
+import domain.Product;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+public interface ProductRepository extends CassandraRepository<Product, Integer> {
+
 }
