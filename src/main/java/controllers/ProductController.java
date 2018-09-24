c3abac14cb5d5453931139695d59bd40038fd60f
DissoLver <indra.elvian@gmail.com>
2018-09-24 17:19:44 +0700

    add gitnore

--- src/main/java/controllers/ProductController.java
+++ src/main/java/controllers/ProductController.java
@@ -3,22 +3,19 @@ package controllers;
 import domain.Product;
 import models.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Qualifier
 @RestController
 public class ProductController{
 
     private ProductRepository productRepository;
 
     @Autowired
-    public ProductController() {
-       // this();
-    }
-
-
     public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
@@ -27,6 +24,6 @@ public class ProductController{
     public List<Product> getAllProduct() {
         //List<User> users = this.userRepository.findAll();
         List<Product> products = this.productRepository.findAll();
-        //return this.productRepository.findAll();
+        return products;
     }
 }
