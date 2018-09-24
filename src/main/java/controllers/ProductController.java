64532679af789c91cc7303d8ce3a6260031980af
DissoLver <indra.elvian@gmail.com>
2018-09-24 13:49:32 +0700

    Add method getAllProduct

--- src/main/java/controllers/ProductController.java
+++ src/main/java/controllers/ProductController.java
@@ -1,15 +1,25 @@
 package controllers;
 
+import domain.Product;
 import models.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class ProductController
+public class ProductController{
     private ProductRepository productRepository;
 
-        @Autowired
-        public ProductController(ProductRepository productRepository) {
-            this.productRepository = productRepository;
-        }
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProduct() {
+        //List<User> users = this.userRepository.findAll();
+        return this.productRepository.findAll();
+    }
 }
