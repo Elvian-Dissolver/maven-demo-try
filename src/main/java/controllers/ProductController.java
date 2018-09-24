39a37be4aff2693a10ee46a6a856303b471c9c52
DissoLver <indra.elvian@gmail.com>
2018-09-24 13:46:42 +0700

    Add attribute object ProductRepository

--- src/main/java/controllers/ProductController.java
+++ src/main/java/controllers/ProductController.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import models.ProductRepository;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProductController {
+public class ProductController
+    private ProductRepository productRepository;
 }
