03ff03d7969d5c0f2757051734c6444aad1936ac
DissoLver <indra.elvian@gmail.com>
2018-09-24 17:28:14 +0700

    add user seeder class

--- src/main/java/models/UserRepository.java
+++ src/main/java/models/UserRepository.java
@@ -1,4 +1,10 @@
 package models;
 
-public interface UserRepository {
+import domain.User;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CassandraRepository<User, Integer> {
+    List<User> findByAgeLessThan(int age);
 }
