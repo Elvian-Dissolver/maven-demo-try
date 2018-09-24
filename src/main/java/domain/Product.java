6c7d7d3926f7f6d6130b4cf746a61c7c23eac690
DissoLver <indra.elvian@gmail.com>
2018-09-24 13:02:16 +0700

    Add method for class Product

--- src/main/java/domain/Product.java
+++ src/main/java/domain/Product.java
@@ -13,4 +13,34 @@ public class Product {
     private String price;
 
     private int stock;
+
+    public Product(){}
+    public Product(int id, String title, String description, String image , String price, int stock) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
