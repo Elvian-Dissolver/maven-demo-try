1c41726e90907da48af791468d930cf38c669011
DissoLver <indra.elvian@gmail.com>
2018-09-24 12:56:56 +0700

    Add attribute for class Product

--- src/main/java/domain/Product.java
+++ src/main/java/domain/Product.java
@@ -1,4 +1,16 @@
 package domain;
 
+
 public class Product {
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private String image;
+
+    private String price;
+
+    private int stock;
 }
