8df1ee9dea595ae038579ae352c5350cf519664a
DissoLver <indra.elvian@gmail.com>
2018-09-24 17:29:08 +0700

    add attribut and method user seeder

--- src/main/java/models/UserSeeder.java
+++ src/main/java/models/UserSeeder.java
@@ -1,4 +1,41 @@
 package models;
 
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table("user")
 public class UserSeeder {
+    @PrimaryKey
+    private int id;
+
+    private String name;
+
+    private String address;
+
+    private int age;
+
+
+    public User(){}
+    public User(int id, String name, String address, int age) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
