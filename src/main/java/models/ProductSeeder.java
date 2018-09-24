17d077cedc6d557fdedd2c5402ca83aeb58780a5
DissoLver <indra.elvian@gmail.com>
2018-09-24 13:24:45 +0700

    change ProductRepository class to implement

--- src/main/java/models/ProductSeeder.java
+++ src/main/java/models/ProductSeeder.java
@@ -1,6 +1,13 @@
 package models;
 
 
-public class ProductSeeder {
+import org.springframework.boot.CommandLineRunner;
 
+public class ProductSeeder implements CommandLineRunner {
+    private ProductRepository productRepository;
+    
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
