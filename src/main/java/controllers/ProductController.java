c017de097f90dc59994fa84a401eb32334277ccf
DissoLver <indra.elvian@gmail.com>
2018-09-24 13:48:06 +0700

    Add contructor ProductController

--- src/main/java/controllers/ProductController.java
+++ src/main/java/controllers/ProductController.java
@@ -1,9 +1,15 @@
 package controllers;
 
 import models.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController
     private ProductRepository productRepository;
+
+        @Autowired
+        public ProductController(ProductRepository productRepository) {
+            this.productRepository = productRepository;
+        }
 }
